b9c2fd371aed3d8ecb23ab27dc171215ad30fad1
Joshua Low <Joshua Low>
2020-12-06 17:06:58 +0800

    Final

--- App.js
+++ App.js
@@ -5,10 +5,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <View style={styles.fillerContainer}></View>
+
+      <View style={styles.titleContainer}>
+
       <Text style={styles.text}>woohoo!!!!</Text>
-      <Text>You got it babe!</Text>
-      <Text>You got it dude!</Text>
+      <Text style={{ fontStyle: "italic", color: "blue"}}>You got it babe!</Text>
+
+      </View>
+
+      <View style={styles.fillerContainer}></View>
+
       <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const ViewStyleProps = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.top} />
+      <View style={styles.middle} />
+      <View style={styles.bottom} />
     </View>
   );
 }
@@ -25,5 +44,20 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
-}
+  },
+  fillerContainer: {
+    padding: 10,
+    backgroundColor: "blue",
+    flex: 0.25,
+    width: "15%",
+  },
+  titleContainer: {
+    backgroundColor: "orange",
+    padding: 10,
+    margin: 10,
+    flex: 0.5,
+    justifyContent: "center",
+    borderRadius: 20,
+    width: "30%",
+  },
 });
